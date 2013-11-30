8e5628440a9d5e0836fe4f6f472be813d6601049
Cole Busby <colebusby@gmail.com>
2013-11-30 10:10:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scott::Application.config.secret_token = 'c564f556fab8f42b98096ab65d1e6d46a3e447ce79aac299941bc3b11723bb6ca0638c64d158f4e9b79143fa4c9fbc56ed5592f66a404c9a2dc8174f8beba261'
